48330293ebaa9e20f68f34720f00b4854f9fc6d5
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2013-11-20 20:04:36 +0100

    Update wallet.py

---
+++ src/wallet/wallet.py
@@ -0,0 +1,19 @@
+"""Cryptocurrency wallet implementation"""
+
+class Wallet:
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
+        self.balance = 0
+
+    def generate_keys(self):
+        """Generate public/private key pair"""
+        # Simplified key generation
+        import random
+        self.private_key = hex(random.getrandbits(256))
+        self.public_key = hex(random.getrandbits(256))
+
+    def get_balance(self):
+        """Get wallet balance"""
+        return self.balance
+Update 1 on 2013-11-20 20:04:36
